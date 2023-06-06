d2ab358c7795b67150fa66b54bcb8be7eca8555b
jasondhakhwa <jasondhakhwa19@gmail.com>
2023-06-06 07:42:19 +0545

    changed cart icon to add to cart button

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addProduct, removeProduct } from '../redux/cartReducer';
 import publicRequest from '../requests/requestMethos';
-
+import bluejeans from "./bluejeans.jpg"
 import { useSearchParams } from 'react-router-dom';
 const ProductListPage = () => {
 
@@ -44,11 +44,14 @@ const ProductListPage = () => {
                             className="bg-white rounded-lg overflow-hidden shadow-lg hover:shadow-2xl transform hover:scale-105 transition-all duration-300 ease-in-out"
                         >
                             <div className="relative h-56">
-                                <img
-                                    src={product.image}
-                                    alt={product.name}
-                                    className="w-full h-full object-cover"
-                                />
+
+                                <div className="w-full h-full filter overflow-hidden hover bg-no-repeat object-fit bg-cover" style={{ backgroundImage: `url(${bluejeans})` }}>
+                                    {/* <img
+                                        src={bluejeans}
+                                        alt={product.name}
+                                        className="w-full h-full object-cover filter hover:blur-s"
+                                    /> */}
+                                </div>
                                 {product.offer ? (
                                     <div className="absolute top-0 right-0 p-2 bg-blue-500 rounded-bl-lg">
                                         <span className="text-white font-semibold line-through mr-1">
@@ -69,26 +72,26 @@ const ProductListPage = () => {
                             <div className="p-4">
                                 <h2 className="text-lg font-semibold text-gray-800 mb-2 flex justify-between items-center">
                                     <span>{product.name}</span>
-                                    {
-                                        cart?.products?.find(item => item._id === product._id) ?
-                                            <button onClick={() => handleRemoveFromCart(product)} >
-                                                <HighlightOffIcon className="w-5 h-5 text-blue-500" />
-                                            </button>
-                                            :
-                                            <button onClick={() => handleAddToCart(product)} >
-                                                <ShoppingCartOutlined className="w-5 h-5 text-blue-500" />
-                                            </button>
-                                    }
                                 </h2>
-                                <p className="text-sm text-gray-600">
+                                <p className="text-sm text-gray-600 mb-2">
                                     {product.description}
                                 </p>
+                                {
+                                    cart?.products?.find(item => item._id === product._id) ?
+                                        <button onClick={() => handleRemoveFromCart(product)} className='hover:bg-blue-600 text-white rounded-sm w-full p-2 bg-blue-500'>
+                                            <HighlightOffIcon className="w-5 h-5" /> Added to cart
+                                        </button>
+                                        :
+                                        <button onClick={() => handleAddToCart(product)} className='hover:bg-blue-600 text-white rounded-sm w-full p-2 bg-blue-500'>
+                                            <ShoppingCartOutlined className="w-5 h-5 " /> Add to cart
+                                        </button>
+                                }
                             </div>
                         </div>
                     );
                 })}
             </div>
-        </div>
+        </div >
     );
 };
 

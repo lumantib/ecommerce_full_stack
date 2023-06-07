3def41eb5fc03706122f4aeadb4824c3c0daef0a
jasondhakhwa <jasondhakhwa19@gmail.com>
2023-06-07 12:14:21 +0545

    added icon to ordered list

--- client/src/components/Admin/AdminPages/Products/AdminViewOrder/columns.js
+++ client/src/components/Admin/AdminPages/Products/AdminViewOrder/columns.js
@@ -1,8 +1,19 @@
+import CheckCircleRoundedIcon from '@mui/icons-material/CheckCircleRounded';
+import CancelRoundedIcon from '@mui/icons-material/CancelRounded';
+
 export const columns = [
     // { field: '_id', headerName: 'ID', minWidth: 250 },
     { field: 'username', headerName: 'Buyer', minWidth: 50 },
     { field: 'email', headerName: 'Contact Email', minWidth: 50 },
     { field: 'products', headerName: 'Items', width: 50, valueFormatter: (params) => params?.value?.length },
     { field: 'totalPrice', headerName: 'Price', width: 50 },
-    { field: 'payement_completed', headerName: 'Payement Completed', minWidth: 50, flex: 1 }
+    {
+        field: 'payement_completed', headerName: 'Payement Completed', minWidth: 50, flex: 1,
+        renderCell: (params) => (
+            params?.value ?
+                <CheckCircleRoundedIcon />
+                :
+                <CancelRoundedIcon />)
+
+    }
 ];

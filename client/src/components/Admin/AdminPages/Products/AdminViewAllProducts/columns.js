bfb08e40213e7ce9defd9466ff62822f5f488424
jasondhakhwa <jasondhakhwa19@gmail.com>
2023-10-16 15:47:43 +0545

    column size

--- client/src/components/Admin/AdminPages/Products/AdminViewAllProducts/columns.js
+++ client/src/components/Admin/AdminPages/Products/AdminViewAllProducts/columns.js
@@ -1,9 +1,9 @@
 export const columns = [
     // { field: '_id', headerName: 'ID', minWidth: 250 },
     { field: 'photo', headerName: 'Image', width: 30, renderCell: (params) => params.value ? <img src={`http://localhost:5000/photo/${params.value}`} className="rounded-full w-8 h-8" /> : <></>, },
-    { field: 'name', headerName: 'Status', minWidth: 50 },
-    { field: 'price', headerName: 'Price', minWidth: 150 },
-    { field: 'isVerified', headerName: 'Verified', minWidth: 150 },
+    { field: 'name', headerName: 'Product Name', minWidth: 200 },
+    { field: 'price', headerName: 'Price', minWidth: 50 },
+    { field: 'isVerified', headerName: 'Verified', minWidth: 50 },
     { field: 'buyer', headerName: 'Buyer', minWidth: 150, valueFormatter: (params) => params?.value?.username },
     { field: 'seller', headerName: 'Seller', minWidth: 150, valueFormatter: (params) => params?.value?.username },
     { field: 'description', headerName: 'Description', minWidth: 150, flex: 1 }

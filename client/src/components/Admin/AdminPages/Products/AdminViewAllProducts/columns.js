9bc478c1a0b93d63263d4c66eea3610cce4bf3b1
jasondhakhwa <jasondhakhwa19@gmail.com>
2023-10-18 19:55:43 +0545

    IMAGE TO IMG

--- client/src/components/Admin/AdminPages/Products/AdminViewAllProducts/columns.js
+++ client/src/components/Admin/AdminPages/Products/AdminViewAllProducts/columns.js
@@ -1,6 +1,6 @@
 export const columns = [
     // { field: '_id', headerName: 'ID', minWidth: 250 },
-    { field: 'photo', headerName: 'Image', width: 30, renderCell: (params) => params.value ? <img src={`http://localhost:5000/photo/${params.value}`} className="rounded-full w-8 h-8" /> : <></>, },
+    { field: 'photo', headerName: 'IMG', width: 30, renderCell: (params) => params.value ? <img src={`http://localhost:5000/photo/${params.value}`} className="rounded-full w-8 h-8" /> : <></>, },
     { field: 'name', headerName: 'Product Name', minWidth: 200 },
     { field: 'price', headerName: 'Price', minWidth: 50 },
     { field: 'isVerified', headerName: 'Verified', minWidth: 50 },
